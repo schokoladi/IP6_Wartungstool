a976547fb37bbf712526cc4467bd963c931cbe0d
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-06-24 15:54:09 +0100

    added angular views

--- public/views/index.php
+++ public/views/index.php
@@ -1,76 +1,91 @@
-<!doctype html>
-<html lang="en">
+<!-- Dokument muss index.php heissen, Laravel-Richtlinie -->
+<!DOCTYPE html>
+<html lang="de">
 <head>
-  <meta charset="UTF-8">
-  <title>Wartungstool</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <!-- CSS -->
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
+  <!-- disable scrolling on mobile -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <!-- Custom styles for this template -->
-    <link href="css/custom.css" rel="stylesheet">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
 
-    <!-- JS -->
-    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.16/angular.min.js"></script> <!-- load angular -->
+  <title>IP6 Wartungstool</title>
 
-    <!-- ANGULAR -->
-    <!-- all angular resources will be loaded from the /public folder -->
-    <script src="js/controllers/mainCtrl.js"></script> <!-- load our controller -->
-    <script src="js/services/produktService.js"></script> <!-- load our service -->
-    <script src="js/app.js"></script> <!-- load our application -->
+  <!-- Bootstrap core CSS -->
+  <link href="styles/bootstrap.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="styles/custom.css" rel="stylesheet">
+
+  <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+  <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+  <!-- <script src="../../assets/js/ie-emulation-modes-warning.js"></script> -->
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
 
-<!-- declare our angular app and controller -->
-<body class="container" ng-app="produktApp" ng-controller="mainController">
-  <div class="col-md-8 col-md-offset-2">
+<!-- controller wird erst in ng-view file defchroiniert -->
+<body ng-app="wartungstoolApp">
 
-    <!-- PAGE TITLE =============================================== -->
-    <div class="page-header">
-        <h2>Laravel and Angular Single Page Application</h2>
-        <h4>Test System</h4>
+  <div ng-include='"templates/navbar.html"'></div>
+  <div class="container">
+    <div class="row">
+      <!-- medium device (Desktop) column -->
+      <!--<div ng-include='"views/produkte.html"'></div>-->
+      <!-- $routeProvider -->
+      <div ng-view></div>
     </div>
 
-    <!-- NEW COMMENT FORM -->
-	<form ng-submit="submitProdukt()"> <!-- ng-submit will disable the default form action and use our function -->
+  </div>
 
-		<!-- AUTHOR -->
-		<div class="form-group">
-			<input type="text" class="form-control input-sm" name="Name" ng-model="produktData.Name" placeholder="Name">
-		</div>
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
 
-		<!-- COMMENT TEXT -->
-		<div class="form-group">
-			<input type="text" class="form-control input-lg" name="Artikelnummer" ng-model="produktData.Artikelnummer" placeholder="Artikelnummer">
-		</div>
-    <div class="form-group">
-			<input type="text" class="form-control input-lg" name="Hersteller_ID" ng-model="produktData.Hersteller_ID" placeholder="Hersteller">
-		</div>
+  <!-- note jquery tag has to go before boostrap -->
+  <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.16/angular.min.js"></script> <!-- load angular -->
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.16/angular-route.min.js"></script>
 
-		<!-- SUBMIT BUTTON -->
-		<div class="form-group text-right">
-			<button type="submit" class="btn btn-primary btn-lg">Submit</button>
-		</div>
-	</form>
+  <!-- ANGULAR -->
+  <!-- all angular resources will be loaded from the /public folder -->
 
-	<pre>
-	{{ produktData }}
-	</pre>
+  <!--<script src="js/controllers/produktController.js"></script>
+  <script src="js/controllers/wartungsvertragController.js"></script>
+  <script src="js/controllers/homeController.js"></script>-->
 
-	<!-- LOADING ICON -->
-	<!-- show loading icon if the loading variable is set to true -->
-	<p class="text-center" ng-show="loading"><span class="fa fa-meh-o fa-5x fa-spin"></span></p>
+  <!-- <script src="js/services/produktService.js"></script> -->
+  <script src="js/app.js"></script> <!-- load our application -->
 
-    <!-- THE COMMENTS =============================================== -->
-    <!-- hide these comments if the loading variable is true -->
-    <div class="produkt" ng-hide="loading" ng-repeat="produkt in produkte">
-        <h3>{{ produkt.Name }}</h3>
-        <p>{{ produkt.Artikelnummer }}</p>
+  <!--<script src="js/jquery-scrolltofixed-min.js" type="text/javascript"></script>
+  <script src="js/custom.js"></script>-->
+  <!--<script>
+  $(document).ready(function() {
+    var stickyNavTop = $('.sticky-top').offset().top;
 
-        <p><a href="#" ng-click="deleteProdukt(produkt.ID)" class="text-muted">Delete</a></p>
-    </div>
+    var stickyNav = function(){
+      var scrollTop = $(window).scrollTop();
 
-</div>
+      if (scrollTop > stickyNavTop) {
+        $('.sticky-top').addClass('sticky');
+      } else {
+        $('.sticky-top').removeClass('sticky');
+      }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function() {
+      stickyNav();
+    });
+  });
+</script>-->
 </body>
 </html>
