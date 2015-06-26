1cebbd8a7fdef2a275f01a4889006d31f3c3b43f
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-06-26 15:44:51 +0100

    fixed routes

--- public/app/app.routing.js
+++ public/app/app.routing.js
@@ -7,13 +7,17 @@ app.config(function($routeProvider){
       templateUrl: "app/components/start/startView.html",
       controller: "startController"
     })
+    .when("/start", {
+      templateUrl: "app/components/start/startView.html",
+      controller: "startController"
+    })
     // produkte
-    .when("/#/produkte", {
+    .when("/produkte", {
       templateUrl: "app/components/product/productView.html",
       controller: "productController"
     })
     // wartungsvertraege
-    .when("/#/wartungsvertraege", {
+    .when("/wartungsvertraege", {
       templateUrl: "app/components/contract/contractView.html",
       controller: "contractController"
     });
