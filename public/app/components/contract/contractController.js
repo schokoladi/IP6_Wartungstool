15d135069c41551bb39a6c6b240e5dcd824e327b
JanSchnider <jan.schnider@gmx.ch>
2015-06-30 16:03:15 +0200

    new controller

--- public/app/components/contract/contractController.js
+++ public/app/components/contract/contractController.js
@@ -3,5 +3,9 @@ app.controller('contractController', function($scope, $http) {
   console.log("Contract Controller loaded.");
   $http.get("/api/contracts").success(function(response) {
     $scope.contracts = response;
+  })
+
+  $http.get("/api/products").success(function(response) {
+    $scope.products = response;
   });
 });
