171b783f1468a5e86830b05c37eceb16023b4ea3
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-07-11 20:14:02 +0100

    edited info page with angular

--- public/app/components/contract/contractController.js
+++ public/app/components/contract/contractController.js
@@ -4,7 +4,12 @@ app.controller('contractController', function($scope, $http) {
   $http.get("/api/contracts").success(function(response) {
     $scope.contracts = response;
   })
-
+  $http.get("/api/customers").success(function(response) {
+    $scope.customers = response;
+  })
+  $http.get("/api/contacts").success(function(response) {
+    $scope.contacts = response;
+  })
   $http.get("/api/products").success(function(response) {
     $scope.products = response;
   });
