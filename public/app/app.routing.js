ac279049e4a1fe3b7d3f3116c90f2187f292ea3a
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-07-13 13:17:41 +0100

    minor changes

--- public/app/app.routing.js
+++ public/app/app.routing.js
@@ -2,48 +2,50 @@
 
 app.config(function($routeProvider){
   $routeProvider
-    // home
-    .when("/", {
-      templateUrl: "app/components/start/startView.html",
-      controller: "startController"
-    })
-    .when("/start", {
-      templateUrl: "app/components/start/startView.html",
-      controller: "startController"
-    })
-    // produkte
-    .when("/produkte", {
-      templateUrl: "app/components/product/productView.html",
-      controller: "productController"
-    })
-    // produkte neu
-    .when("/produkte/neu", {
-      templateUrl: "app/components/product/productEdit.html",
-      controller: "productController"
-    })
-    // produkte neu
-    .when("/produkte/:ID/edit", {
-      templateUrl: "app/components/product/productEdit.html",
-      controller: "productController"
-    })
-    // wartungsvertraege
-    .when("/wartungsvertraege", {
-      templateUrl: "app/components/contract/contractView.html",
-      controller: "contractController"
-    })
-    // contract new
-    .when("/wartungsvertraege/neu", {
-      templateUrl: "app/components/contract/contractNewInfo.html",
-      controller: "contractController"
-    })
-    // contract new article
-    .when("/wartungsvertraege/neu/artikel", {
-      templateUrl: "app/components/contract/contractNewArticle.html",
-      controller: "contractController"
-    })
-    // contract new article
-    .when("/wartungsvertraege/neu/stundenpool", {
-      templateUrl: "app/components/contract/contractNewPool.html",
-      controller: "contractController"
-    });
+  // Start pages
+  .when("/", {
+    templateUrl: "app/components/start/startView.html",
+    controller: "startController"
+  })
+  .when("/start", {
+    templateUrl: "app/components/start/startView.html",
+    controller: "startController"
+  })
+
+  // produkte
+  .when("/produkte", {
+    templateUrl:  "app/components/product/productView.html",
+    controller:   "productController"
+  })
+  // produkte neu
+  .when("/produkte/neu", {
+    templateUrl:  "app/components/product/productEdit.html",
+    controller:   "productController"
+  })
+  // produkte neu
+  .when("/produkte/:ID/edit", {
+    templateUrl:  "app/components/product/productEdit.html",
+    controller:   "productController"
+  })
+
+  // wartungsvertraege
+  .when("/wartungsvertraege", {
+    templateUrl: "app/components/contract/contractView.html",
+    controller: "contractController"
+  })
+  // contract new
+  .when("/wartungsvertraege/neu", {
+    templateUrl: "app/components/contract/contractNewInfo.html",
+    controller: "contractController"
+  })
+  // contract new article
+  .when("/wartungsvertraege/neu/artikel", {
+    templateUrl: "app/components/contract/contractNewArticle.html",
+    controller: "contractController"
+  })
+  // contract new article
+  .when("/wartungsvertraege/neu/stundenpool", {
+    templateUrl: "app/components/contract/contractNewPool.html",
+    controller: "contractController"
+  });
 });
