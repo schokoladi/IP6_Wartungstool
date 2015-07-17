d2311c2f1590e9db61301bb93f13e5466e4f693a
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-07-17 15:11:56 +0100

    added functions to get data from related models

--- app/Contract.php
+++ app/Contract.php
@@ -10,4 +10,26 @@ class Contract extends Model
   protected $primaryKey = 'ID';
 
   public $timestamps = true;
+
+  public function customer()
+  {
+    return $this->hasOne('App\Customer', 'ID', 'Wartungsvertraege_Kunden_ID');
+  }
+
+  public function contact()
+  {
+    return $this->hasOne('App\Contact', 'ID', 'Wartungsvertraege_Kontaktpersonen_ID');
+  }
+
+  // Funktion zum Holen der zum Wartungsvertrag gehörigen Artikel
+  public function articles()
+  {
+    return $this->hasMany('App\Article', 'Artikel_Wartungsvertraege_ID', 'ID');
+  }
+
+  public function pools()
+  {
+    return $this->hasMany('App\Pool', 'Stundenpools_Wartungsvertraege_ID', 'ID');
+  }
+
 }
