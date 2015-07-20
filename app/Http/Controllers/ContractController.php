8da707653e563f5226bb24974d37ddff13eeab99
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-07-20 17:15:32 +0100

    added if-clause for show()

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -76,86 +76,92 @@ class ContractController extends Controller
     return response()->json([
       'success' => true,
       'Contract' => Contract::orderBy('ID', 'desc')->first()
-    ]);
+      ]);
 
-  }
-
-  /**
-  * Display the specified resource.
-  *
-  * @param  int  $id
-  * @return Response
-  */
-  public function show($id)
-  {
-    $contract = Contract::findOrFail($id);
-    $json = array();
-    $i = 0;
-    $j = 0;
-
-    $json['ID'] = $contract->ID;
-    $json['Vertragsnummer'] = $contract->Vertragsnummer;
-    $json['Beschreibung'] = $contract->Beschreibung;
-    $json['Inaktiv'] = $contract->Inaktiv;
-    $json['Zustaendigkeit'] = $contract->Zustaendigkeit;
-    // Kunde und Kontaktperson via ID holen
-    $json['Kunde'] = $contract->customer->Name;
-    $json['Kontaktperson_Vorname'] = $contract->contact->Vorname;
-    $json['Kontaktperson_Name'] = $contract->contact->Name;
-
-    $articles = Contract::find($id)->articles;
-    foreach($articles as $article) {
-      $json['Artikel'][$i]['Seriennummer'] = $article->Seriennummer;
-      $json['Artikel'][$i]['Artikelnummer'] = $article->product->Artikelnummer;
-      $json['Artikel'][$i]['Name'] = $article->product->Name;
-      $json['Artikel'][$i]['Hersteller'] = $article->product->manufacturer->Name;
-      $json['Artikel'][$i]['Maintenance'] = $article->maintenance->Maintenance;
-      $json['Artikel'][$i]['Start'] = $article->Maintenance_Start;
-      $i++;
     }
 
-    $pools = Contract::find($id)->pools;
-    foreach($pools as $pool) {
-      $json['Stundenpools'][$j]['Stundenpool_Start'] = $pool->Stundenpool_Start;
-      $json['Stundenpools'][$j]['Anzahl_Stunden'] = $pool->Anzahl_Stunden;
-      $json['Stundenpools'][$j]['Stundensatz'] = $pool->Stundensatz;
-      $json['Stundenpools'][$j]['Rechnungsnummer'] = $pool->Rechnungsnummer;
-      $j++;
+    /**
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function show($id)
+    {
+      $contract = Contract::findOrFail($id);
+      $json = array();
+      $i = 0;
+      $j = 0;
+
+      $json['ID'] = $contract->ID;
+      $json['Vertragsnummer'] = $contract->Vertragsnummer;
+      $json['Beschreibung'] = $contract->Beschreibung;
+      $json['Inaktiv'] = $contract->Inaktiv;
+      $json['Zustaendigkeit'] = $contract->Zustaendigkeit;
+      // Kunde und Kontaktperson via ID holen
+      $json['Kunde'] = $contract->customer->Name;
+      $json['Kontaktperson_Vorname'] = $contract->contact->Vorname;
+      $json['Kontaktperson_Name'] = $contract->contact->Name;
+
+      $articles = Contract::find($id)->articles;
+      // Artikel nür füllen, wenn Werte vorhanden
+      if(!empty($articles)) {
+        foreach($articles as $article) {
+          $json['Artikel'][$i]['Seriennummer'] = $article->Seriennummer;
+          $json['Artikel'][$i]['Artikelnummer'] = $article->product->Artikelnummer;
+          $json['Artikel'][$i]['Name'] = $article->product->Name;
+          $json['Artikel'][$i]['Hersteller'] = $article->product->manufacturer->Name;
+          $json['Artikel'][$i]['Maintenance'] = $article->maintenance->Maintenance;
+          $json['Artikel'][$i]['Start'] = $article->Maintenance_Start;
+          $i++;
+        }
+      }
+
+      $pools = Contract::find($id)->pools;
+      // Pools nür füllen wenn Werte vorhanden
+      if(!empty($pools)) {
+        foreach($pools as $pool) {
+          $json['Stundenpools'][$j]['Stundenpool_Start'] = $pool->Stundenpool_Start;
+          $json['Stundenpools'][$j]['Anzahl_Stunden'] = $pool->Anzahl_Stunden;
+          $json['Stundenpools'][$j]['Stundensatz'] = $pool->Stundensatz;
+          $json['Stundenpools'][$j]['Rechnungsnummer'] = $pool->Rechnungsnummer;
+          $j++;
+        }
+      }
+
+      return response()->json($json);
     }
 
-    return response()->json($json);
-  }
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function edit($id)
+    {
+      //
+    }
 
-  /**
-  * Show the form for editing the specified resource.
-  *
-  * @param  int  $id
-  * @return Response
-  */
-  public function edit($id)
-  {
-    //
-  }
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function update($id)
+    {
+      //
+    }
 
-  /**
-  * Update the specified resource in storage.
-  *
-  * @param  int  $id
-  * @return Response
-  */
-  public function update($id)
-  {
-    //
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function destroy($id)
+    {
+      //
+    }
   }
-
-  /**
-  * Remove the specified resource from storage.
-  *
-  * @param  int  $id
-  * @return Response
-  */
-  public function destroy($id)
-  {
-    //
-  }
-}
