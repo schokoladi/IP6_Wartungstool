035571e9d219c9799aa35efb2c6bf12c58f06707
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-07-22 11:08:42 +0100

    added fillable options

--- app/Article.php
+++ app/Article.php
@@ -9,6 +9,34 @@ class Article extends Model
   protected $table = 'Artikel';
   protected $primaryKey = 'ID';
 
+  protected $fillable = [
+    'Artikel_Wartungsvertraege_ID',
+    'Artikel_Produkte_ID',
+    'Seriennummer',
+    'EKP_Artikel',
+    'EKP_Artikel_Waehrungen_ID',
+    'VKP_Artikel',
+    'VKP_Artikel_Waehrungen_ID',
+    'Auftragsnummer',
+    'Auftragsdatum',
+    'Rechnungsnummer',
+    'Rechnungsdatum',
+    'Artikel_Maintenance_ID',
+    'Maintenance_Start',
+    'Maintenance_von',
+    'Maintenance_bis',
+    'EKP_Maintenance',
+    'EKP_Maintenance_Waehrungen_ID',
+    'VKP_Maintenance',
+    'VKP_Maintenance_Waehrungen_ID',
+    'Artikel_Operationsupport_ID',
+    'Operationsupport_Start',
+    'Operationsupport_von',
+    'Operationsupport_bis',
+    'VKP_Operationsupport',
+    'VKP_Operationsupport_Waehrungen_ID'
+  ];
+
   public $timestamps = true;
 
 
