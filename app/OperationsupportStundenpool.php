8a50c201e14bbf69148b75a09927cda4f47e5479
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-07-24 11:05:52 +0100

    added OSPool Controller

---
+++ app/OperationsupportStundenpool.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OperationsupportStundenpool extends Model
+{
+  protected $table = 'Operationsupport_Stundenpools';
+  protected $primaryKey = 'ID';
+
+  public $timestamps = true;
+}
