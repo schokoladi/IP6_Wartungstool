1ae86fd30e41b39a171fde71f86a863ee5374bbb
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-07-26 22:53:31 +0100

    added upload page, not working

--- public/app/components/upload/uploadController.js
+++ public/app/components/upload/uploadController.js
@@ -1,3 +1,13 @@
-app.controller('uploadController', function($scope, $http, $location, $routeParams) {
-  // TODO
+console.log('upload controller loaded');
+
+app.controller('uploadController',
+function($scope, $http, $location) {
+
+  console.log($scope.uploadfile);
+  Papa.parse($scope.uploadfile, {
+    complete: function(results) {
+      console.log("Finished:", results.data);
+    }
+  });
+
 });
