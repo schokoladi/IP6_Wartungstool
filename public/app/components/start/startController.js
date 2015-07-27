529187fd308d89bdf6fb5ba30d3ddd05c14d6514
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-07-27 17:15:14 +0100

    no functionality

--- public/app/components/start/startController.js
+++ public/app/components/start/startController.js
@@ -1,10 +1,7 @@
+console.log('start controller loaded');
+
 // controller for start page
 // necessary????
 app.controller('startController', function($scope, $http, $rootScope) {
-  console.log("Start Controller loaded.");
-  /*
-  $http.get('/api/start').success(function(response) {
-    $scope.produkte = response;
-  });
-  */
+  console.log($rootScope.authenticated);
 });
