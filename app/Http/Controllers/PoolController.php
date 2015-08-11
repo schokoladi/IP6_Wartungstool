60b0421c2babd03e30a410648f473fd6f41df999
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-08-11 15:15:58 +0100

    refactoring, bug fixed (Input::get)

--- app/Http/Controllers/PoolController.php
+++ app/Http/Controllers/PoolController.php
@@ -11,16 +11,22 @@ use App\Http\Controllers\Controller;
 use Input;
 use DateTime;
 
+/**
+ * Die PoolController-Klasse handlet alle Funktionen (Actions), welche über
+ * die URL 'api/stundenpools' aufgerufen werden
+ */
 class PoolController extends Controller
 {
-  public function __construct() {
-    // für alle
-    //$this->middleware('jwt.auth');
-    // Mit Ausnahmen
-    $this->middleware('jwt.auth');
 
-    // Wird dann so in den routes angezeigt!!!
+  /**
+  * Mit dem Konstruktor wird diese Klasse in der Middleware registriert, welche
+  * beim Seitenaufruf zwischengeschaltet wird und filtert
+  */
+  public function __construct()
+  {
+    $this->middleware('jwt.auth');
   }
+
   /**
   * Display a listing of the resource.
   *
@@ -42,14 +48,99 @@ class PoolController extends Controller
   }
 
   /**
-  * Store a newly created resource in storage.
+  * Speichert einen erfassten Stundenpool anhand der mit Input übergebenen Formular-
+  * daten.
   *
-  * @param  Request  $request
-  * @return Response
+  * @return response  Success-Meldung als JSON-String
+  * @author Dominik Schoch <dominik.schoch@students.fhnw.ch>
   */
-  public function store(Request $request)
+  public function store()
   {
     $pool = new Pool;
+
+    $pool->Stundenpools_Wartungsvertraege_ID = Input::get('Stundenpools_Wartungsvertraege_ID');
+    $pool->Stundenpools_OS_Stundenpools_ID = Input::get('Stundenpools_OS_Stundenpools_ID');
+
+    $pool->Stundenpool_Start = Input::get('Stundenpool_Start');
+    $pool->Stundenpool_von = Input::get('Stundenpool_von');
+    $pool->Stundenpool_bis = Input::get('Stundenpool_bis');
+
+    $pool->Stundensatz = Input::get('Stundensatz');
+    $pool->Stundensatz_Waehrungen_ID = Input::get('Stundensatz_Waehrungen_ID');
+    $pool->Anzahl_Stunden = Input::get('Anzahl_Stunden');
+
+    $pool->Rechnungsnummer = Input::get('Rechnungsnummer');
+    $pool->Rechnungsdatum = Input::get('Rechnungsdatum');
+
+    $pool->created_at = new DateTime;
+    $pool->updated_at = new DateTime;
+
+    $pool->save();
+
+    return response()->json(['success' => true]);
+  }
+
+  /**
+  * Gibt ein Stundenpool anhand der übergebenen Wartungsvertrags-ID zurück.
+  * Dabei wird die zum Stundenpool gehörigen Operation Supports mitgegeben.
+  * Dies ist momentan nur einer, soll aber erweitert werden können.
+  *
+  * @param  integer   Übergebene ID des Wartungsvertrages
+  * @return response  Generiertes Array als JSON-String
+  * @author Dominik Schoch <dominik.schoch@students.fhnw.ch>
+  */
+  public function show($id)
+  {
+    $pools = Pool::where('Stundenpools_Wartungsvertraege_ID', $id)->get();
+    $json = array();
+    $i = 0;
+
+    // Das Array wird mit den OS-Pools neu zusammengestellt
+    foreach($pools as $pool)
+    {
+      $json[$i]['ID'] = $pool->ID;
+      $json[$i]['Anzahl_Stunden'] = $pool->Anzahl_Stunden;
+      // Hol den zum Stundenpool gehörigen Operation Support
+      $json[$i]['OS_Pool'] = $pool->ospool->Bezeichnung;
+      $i++;
+    }
+
+    return response()->json($json);
+  }
+
+  /**
+  * Gibt einen Stundenpool anhand der übergebenen ID zurück und wandelt dabei die
+  * Daten in das Format "TT.MM.JJJJ" um, da sie in der Datenbank in YYYY-MM-DD
+  * gespeichert sind.
+  *
+  * @param  integer   Übergebene ID des Stundenpools
+  * @return response  Generiertes Array als JSON-String
+  * @author Dominik Schoch <dominik.schoch@students.fhnw.ch>
+  */
+  public function edit($id)
+  {
+    $pool = Pool::findOrFail($id);
+
+    // Formatiere das Datum wieder um (die DB gibt YYYY-MM-DD zurück)
+    $pool->Stundenpool_Start = date("d.m.Y", strtotime($pool->Stundenpool_Start));
+    $pool->Stundenpool_von = date("d.m.Y", strtotime($pool->Stundenpool_von));
+    $pool->Stundenpool_bis = date("d.m.Y", strtotime($pool->Stundenpool_bis));
+    $pool->Rechnungsdatum = date("d.m.Y", strtotime($pool->Rechnungsdatum));
+
+    return response()->json($pool);
+  }
+
+  /**
+  * Aktualisiert einen Stundenpool anhand der mit Input übergebenen Formulardaten.
+  *
+  * @param  Request   Input-Daten
+  * @param  integer   Übergebene ID des Stundenpools
+  * @return response  Success-Meldung JSON-String
+  * @author Dominik Schoch <dominik.schoch@students.fhnw.ch>
+  */
+  public function update(Request $request, $id)
+  {
+    $pool = Pool::find($id);
 
     $pool->Stundenpools_Wartungsvertraege_ID = Input::get('Stundenpools_Wartungsvertraege_ID');
     $pool->Stundenpools_OS_Stundenpools_ID = Input::get('Stundenpools_OS_Stundenpools_ID');
@@ -71,86 +162,11 @@ class PoolController extends Controller
   }
 
   /**
-  * Display the specified resource.
+  * Löscht den per ID übergebenen Stundenpool.
   *
-  * @param  int  $id
-  * @return Response
-  */
-  public function show($id)
-  {
-    $pools = Pool::where('Stundenpools_Wartungsvertraege_ID', $id)->get();
-    $json = array();
-    $i = 0;
-
-    // Das Array wird mit dem Hersteller neu zusammengestellt
-    foreach($pools as $pool)
-    {
-      $json[$i]['ID'] = $pool->ID;
-      $json[$i]['Anzahl_Stunden'] = $pool->Anzahl_Stunden;
-      // Hol den zum Produkt gehörigen Hersteller
-      //$manufacturer = Product::find($product->ID);
-      $json[$i]['OS_Pool'] = $pool->ospool->Bezeichnung;
-      $i++;
-    }
-    // gib das Array als json-String
-    return response()->json($json);
-  }
-
-  /**
-  * Show the form for editing the specified resource.
-  *
-  * @param  int  $id
-  * @return Response
-  */
-  public function edit($id)
-  {
-    $pool = Pool::findOrFail($id);
-
-    // Formatiere das Datum wieder um (die DB gibt YYYY-MM-DD zurück)
-    //$article->Auftragsdatum = Carbon::createFromDate('d.m.Y', $article->Auftragsdatum);
-    $pool->Stundenpool_Start = date("d.m.Y", strtotime($pool->Stundenpool_Start));
-    $pool->Stundenpool_von = date("d.m.Y", strtotime($pool->Stundenpool_von));
-    $pool->Stundenpool_bis = date("d.m.Y", strtotime($pool->Stundenpool_bis));
-    $pool->Rechnungsdatum = date("d.m.Y", strtotime($pool->Rechnungsdatum));
-
-    return response()->json($pool);
-  }
-
-  /**
-  * Update the specified resource in storage.
-  *
-  * @param  Request  $request
-  * @param  int  $id
-  * @return Response
-  */
-  public function update(Request $request, $id)
-  {
-    $pool = Pool::find($id);
-
-    $pool->Stundenpools_Wartungsvertraege_ID = $request->input('Stundenpools_Wartungsvertraege_ID');
-    $pool->Stundenpools_OS_Stundenpools_ID = $request->input('Stundenpools_OS_Stundenpools_ID');
-
-    $pool->Stundenpool_Start = $request->input('Stundenpool_Start');
-    $pool->Stundenpool_von = $request->input('Stundenpool_von');
-    $pool->Stundenpool_bis = $request->input('Stundenpool_bis');
-
-    $pool->Stundensatz = $request->input('Stundensatz');
-    $pool->Stundensatz_Waehrungen_ID = $request->input('Stundensatz_Waehrungen_ID');
-    $pool->Anzahl_Stunden = $request->input('Anzahl_Stunden');
-
-    $pool->Rechnungsnummer = $request->input('Rechnungsnummer');
-    $pool->Rechnungsdatum = $request->input('Rechnungsdatum');
-
-    $pool->save();
-
-    return response()->json(['success' => true]);
-  }
-
-  /**
-  * Remove the specified resource from storage.
-  *
-  * @param  int  $id
-  * @return Response
+  * @param  integer   Übergebene ID des Stundenpools
+  * @return response  Success-Meldung JSON-String
+  * @author Dominik Schoch <dominik.schoch@students.fhnw.ch>
   */
   public function destroy($id)
   {
