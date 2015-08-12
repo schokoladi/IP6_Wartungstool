3a3af5c423e31591c6a8700915838e8b9a3b3c78
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2015-08-12 14:02:39 +0100

    refactoring, added comments

--- public/app/components/upload/uploadController.js
+++ public/app/components/upload/uploadController.js
@@ -1,13 +1,19 @@
-console.log('upload controller loaded');
+//console.log('upload controller loaded');
 
+/**
+* Der uploadController soll den upload des aus Inntact exportierten csv-Files
+* handhaben.
+*/
 app.controller('uploadController',
 function($scope, $http, $location) {
 
+  /*
   console.log($scope.uploadfile);
   Papa.parse($scope.uploadfile, {
     complete: function(results) {
       console.log("Finished:", results.data);
     }
   });
+  */
 
 });
